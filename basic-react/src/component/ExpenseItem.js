5bcb0bb730c27d8ae69e2e6269bb68647c193d73
PriyaNayak28 <nayakpriya612@gmail.com>
2025-04-13 05:20:32 +0530

    Writing Complex JSX

--- basic-react/src/component/ExpenseItem.js
+++ basic-react/src/component/ExpenseItem.js
@@ -1,9 +1,15 @@
 export default function ExpenseItem() {
+  let currentDate = new Date().toString()
   return (
     <>
-      <p>Food - $5</p>
+      {/* <p>Food - $5</p>
       <p>Petrol - $20</p>
-      <p>Movie - $10</p>
+      <p>Movie - $10</p> */}
+      <div>
+        <div>{currentDate}</div>
+        <div>Book-$10</div>
+        <div>Delhi</div>
+      </div>
     </>
   )
 }
