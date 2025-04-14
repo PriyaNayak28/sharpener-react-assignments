f8743753671be8970779765b364e6dcdaec22198
PriyaNayak28 <nayakpriya612@gmail.com>
2025-04-15 03:55:11 +0530

    Listening for Events

--- advance-react/src/Expenses/ExpenseItem.js
+++ advance-react/src/Expenses/ExpenseItem.js
@@ -3,12 +3,16 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 function ExpenseItem(props) {
+  function buttonHandler(event) {
+    console.log('Button clicked')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonHandler}>Change Title</button>
       </div>
     </Card>
   )
