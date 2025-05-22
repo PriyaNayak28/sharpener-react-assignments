92d29934134160ef2bec6d626a32da47302b68cc
PriyaNayak28 <nayakpriya612@gmail.com>
2025-05-23 02:25:13 +0530

    useState Hook

--- advance-react/src/Expenses/ExpenseItem.js
+++ advance-react/src/Expenses/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+import { useState } from 'react'
 
 function ExpenseItem(props) {
+  let [title , setTitle] = useState(" ");
   function buttonHandler(event) {
-    console.log('Button clicked')
+    console.log('Button clicked');
+    setTitle("new Title")
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
         <button onClick={buttonHandler}>Change Title</button>
       </div>
